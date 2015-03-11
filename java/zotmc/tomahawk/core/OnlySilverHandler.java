121ad3f0e90b594cf41c0d77e944ee5eb66c1072
mes <mes.goruba@gmail.com>
2015-03-12 02:47:48 +0900

    fix: can't pick up axe entity when thrower after dead or after 60sec and relogin

--- java/zotmc/tomahawk/core/OnlySilverHandler.java
+++ java/zotmc/tomahawk/core/OnlySilverHandler.java
@@ -16,27 +16,27 @@ import com.google.common.base.Optional;
 import com.google.common.reflect.Reflection;
 
 class OnlySilverHandler<InUseWeapon> implements Function<DamageSource, InUseWeapon> {
-	
+
 	static void init() throws Throwable {
 		Class.forName(OnlySilver.ONLY_SILVER_REGISTRY)
 			.getMethod(OnlySilver.REGISTER_WEAPON_FUNCTION, String.class, Function.class)
 			.invoke(null, "thrown", new OnlySilverHandler<>());
 	}
-	
-	
+
+
 	private final Class<InUseWeapon> inUseWeaponType;
-	
+
 	@SuppressWarnings("unchecked")
 	private OnlySilverHandler() throws Throwable {
 		inUseWeaponType = (Class<InUseWeapon>) Class.forName(OnlySilver.IN_USE_WEAPON);
 	}
-	
+
 	@Override public InUseWeapon apply(DamageSource input) {
 		Entity projectile = input.getSourceOfDamage();
-		
+
 		return !(projectile instanceof EntityTomahawk) ? null : newInUseWeapon((EntityTomahawk) projectile);
 	}
-	
+
 	private final InUseWeapon newInUseWeapon(final EntityTomahawk hawk) {
 		return Reflection.newProxy(
 				inUseWeaponType,
@@ -45,16 +45,16 @@ class OnlySilverHandler<InUseWeapon> implements Function<DamageSource, InUseWeap
 						switch(method.getName()) {
 						case "getUser":
 							return getUser(hawk);
-							
+
 						case "getItem":
 							return getItem(hawk);
-							
+
 						case "update":
 							return update(hawk, (ItemStack) args[0]);
-							
+
 						case "toString":
 							return OnlySilverHandler.this.toString(hawk);
-							
+
 						default:
 							throw new UnsupportedOperationException();
 						}
@@ -62,29 +62,29 @@ class OnlySilverHandler<InUseWeapon> implements Function<DamageSource, InUseWeap
 				}
 		);
 	}
-	
+
 	private Optional<EntityLivingBase> getUser(EntityTomahawk hawk) {
-		return Utils.tryCast(EntityLivingBase.class, hawk.shootingEntity);
+		return Utils.tryCast(EntityLivingBase.class, hawk.getShootingEntity());
 	}
-	
+
 	private Optional<ItemStack> getItem(EntityTomahawk hawk) {
 		return Optional.fromNullable(hawk.item.get());
 	}
-	
+
 	private Void update(EntityTomahawk hawk, ItemStack item) {
 		if (item == null)
 			hawk.onBroken();
-		
+
 		if (item != hawk.item.get())
 			hawk.item.set(item);
-		
+
 		return null;
 	}
-	
+
 	private String toString(EntityTomahawk hawk) {
 		return String.format("[Tomahawk %s thrown by %s]",
 				getItem(hawk).orNull(), getUser(hawk).orNull()
 		);
 	}
-	
+
 }
--- java/zotmc/tomahawk/core/TomahawkImpls.java
+++ java/zotmc/tomahawk/core/TomahawkImpls.java
@@ -34,23 +34,23 @@ import com.google.common.base.Objects;
 import com.google.common.base.Strings;
 
 public class TomahawkImpls {
-	
+
 	/**
 	 * @see EntityPlayer#attackTargetEntityWithCurrentItem
 	 */
 	public static void onEntityImpactImpl(EntityTomahawk hawk, Vec3d motion, Entity target, Random rand) {
 		ItemStack item = hawk.item.get();
-		
+
 		boolean handleAttack = true;
 		if (!hawk.worldObj.isRemote && hawk.worldObj instanceof WorldServer) {
 			FakePlayerTomahawk fakePlayer = hawk.createFakePlayer((WorldServer) hawk.worldObj);
-			
+
 			handleAttack = !MinecraftForge.EVENT_BUS.post(new AttackEntityEvent(fakePlayer, target))
 					&& !item.getItem().onLeftClickEntity(item, fakePlayer, target);
 		}
-		
+
 		if (handleAttack && target.canAttackWithItem() && !target.hitByEntity(hawk)) {
-			
+
 			float damage = hawk.damageAttr;
 			float enchCrit = 0;
 			int knock = hawk.knockbackStr;
@@ -58,29 +58,29 @@ public class TomahawkImpls {
 				enchCrit = Utils.getEnchantmentModifierLiving(item, (EntityLivingBase) target);
 				knock += EnchantmentHelper.getEnchantmentLevel(knockback.effectId, item);
 			}
-			
+
 			if (damage > 0 || enchCrit > 0) {
 				boolean critical = hawk.getIsCritical() && target instanceof EntityLivingBase;
 				if (critical && damage > 0)
 					damage *= 1.5F;
-				
+
 				damage += enchCrit;
-				
+
 				boolean setFire = false;
 				int fire = EnchantmentHelper.getEnchantmentLevel(fireAspect.effectId, item);
 				if (fire > 0 && target instanceof EntityLivingBase && !target.isBurning()) {
 					setFire = true;
 					target.setFire(1);
 				}
-				
-				
-				Entity thrower = hawk.shootingEntity;
-				
+
+
+				Entity thrower = hawk.getShootingEntity();
+
 				boolean attacked = target.attackEntityFrom(
 						new DamageSourceTomahawk(hawk, thrower != null ? thrower : hawk),
 						damage
 				);
-				
+
 				if (attacked) {
 					if (knock > 0) {
 						float hv = motion.norm();
@@ -89,22 +89,22 @@ public class TomahawkImpls {
 								0.1,
 								(float) motion.z() / hv * knock * 0.5F
 						);
-						
+
 						motion.multiplyHorz(0.6);
 					}
-					
+
 					if (critical)
 						onCritical(hawk, target);
-					
+
 					if (enchCrit > 0)
 						onEnchantmentCritical(hawk, target);
-					
+
 					if (damage >= 18 && thrower instanceof EntityPlayer)
 						((EntityPlayer) thrower).triggerAchievement(AchievementList.overkill);
-					
+
 					//setLastAttacker?
 					//hurt player i.e. thorns
-					
+
 					if (thrower instanceof EntityLivingBase)
 						try {
 							Utils.applyEnchantmentDamageIterator(
@@ -113,96 +113,96 @@ public class TomahawkImpls {
 						} catch (Throwable e) {
 							TomahawksCore.instance.log.catching(e);
 						}
-					
+
 					Entity entity = target;
 					if (target instanceof EntityDragonPart) {
 						IEntityMultiPart dragon = ((EntityDragonPart) target).entityDragonObj;
 						if (dragon != null && dragon instanceof EntityLivingBase)
 							entity = (Entity) dragon;
 					}
-					
+
 					if (item != null && entity instanceof EntityLivingBase) {
 						if (thrower instanceof EntityPlayer)
 							item.hitEntity((EntityLivingBase) entity, (EntityPlayer) thrower);
 						else
 							item.attemptDamageItem(2, rand);
-						
+
 						if (item.stackSize <= 0)
 							hawk.onBroken();
 					}
-					
+
 					if (target instanceof EntityLivingBase) {
 						//addStat
-						
+
 						if (fire > 0)
 							target.setFire(fire * 4);
 					}
-					
+
 				}
 				else if (setFire)
 					target.extinguish();
-				
+
 			}
 		}
 	}
-	
+
 	private static void onCritical(Entity attacker, Entity victim) {
 		if (attacker.worldObj instanceof WorldServer)
 			((WorldServer) attacker.worldObj).getEntityTracker().func_151248_b(
 					attacker, new S0BPacketAnimation(victim, 4)
 			);
 	}
-	
+
 	private static void onEnchantmentCritical(Entity attacker, Entity victim) {
 		if (attacker.worldObj instanceof WorldServer)
 			((WorldServer) attacker.worldObj).getEntityTracker().func_151248_b(
 					attacker, new S0BPacketAnimation(victim, 5)
 			);
 	}
-	
-	
+
+
 	/**
 	 * @see EntityLivingBase#renderBrokenItemStack
 	 */
 	public static void renderBrokenItemStack(Entity entity, ItemStack item, Random rand) {
 		entity.playSound("random.break", 0.8F, 0.8F + entity.worldObj.rand.nextFloat() * 0.4F);
-		
+
 		for (int i = 0; i < 5; ++i) {
 			Vec3 v0 = Utils.vec3((rand.nextFloat() - 0.5) * 0.1, Math.random() * 0.1 + 0.1, 0);
 			v0.rotateAroundX(-entity.rotationPitch * Utils.PI / 180.0F);
 			v0.rotateAroundY(-entity.rotationYaw * Utils.PI / 180.0F);
-			
+
 			Vec3 v1 = Utils.vec3((rand.nextFloat() - 0.5) * 0.3, -rand.nextFloat() * 0.6 - 0.3, 0.6);
 			v1.rotateAroundX(-entity.rotationPitch * Utils.PI / 180.0F);
 			v1.rotateAroundY(-entity.rotationYaw * Utils.PI / 180.0F);
 			v1 = v1.addVector(entity.posX, entity.posY + entity.getEyeHeight(), entity.posZ);
-			
+
 			entity.worldObj.spawnParticle("iconcrack_" + Item.getIdFromItem(item.getItem()),
 					v1.xCoord, v1.yCoord, v1.zCoord, v0.xCoord, v0.yCoord + 0.05, v0.zCoord
 			);
 		}
 	}
-	
-	
+
+
 	/**
 	 * @see ContainerRepair#updateRepairOutput
 	 */
 	public static void fuseGoldenSword(AnvilUpdateEvent event, Map<Integer, Integer> additionalEnchs) {
 		event.output = event.left.copy();
 		int c = event.left.getRepairCost();
-		
+
 		Map<Integer, Integer> enchs = Utils.getEnchs(event.output);
 		int i = 0;
-		
+
 		for (Entry<Integer, Integer> entry : additionalEnchs.entrySet()) {
 			int id = entry.getKey();
 			Enchantment ench = Enchantment.enchantmentsList[id];
-			
+
 			int l = Objects.firstNonNull(enchs.get(id), 0);
 			int r = entry.getValue();
 			int lvl = l == r ? r + 1 : Math.max(l, r);
 			int gain = lvl - l;
-			
+
 			boolean applicable = true;
 			for (Entry<Integer, Integer> entry1 : enchs.entrySet()) {
 				int id1 = entry1.getKey();
@@ -211,13 +211,13 @@ public class TomahawkImpls {
 					i += gain;
 				}
 			}
-			
+
 			if (applicable) {
 				enchs.put(id, Math.max(lvl, ench.getMaxLevel()));
 				i += getCostPerLvl(ench.getWeight()) * gain;
 			}
 		}
-		
+
 		int j = 0;
 		if (Strings.isNullOrEmpty(event.name)) {
 			if (event.left.hasDisplayName()) {
@@ -229,49 +229,49 @@ public class TomahawkImpls {
 		else if (!event.name.equals(event.left.getDisplayName())) {
 			j = event.left.isItemStackDamageable() ? 7 : event.left.stackSize * 5;
 			i += j;
-			
+
 			if (event.left.hasDisplayName())
 				c += j / 2;
-			
+
 			event.output.setStackDisplayName(event.name);
 		}
-		
+
 		int count = 0;
 		for (Entry<Integer, Integer> entry : enchs.entrySet())
 			c += ++count + entry.getValue()
 					* getCostPerLvl(Enchantment.enchantmentsList[entry.getKey()].getWeight());
-		
+
 		c = Math.max(1, c / 2);
-		
+
 		try {
 			if (!event.output.getItem().isBookEnchantable(event.output, event.right))
 				event.output = null;
 		} catch (Throwable e) {
 			TomahawksCore.instance.log.catching(e);
 		}
-		
+
 		event.cost = c + i;
-		
+
 		if (i <= 0)
 			event.output = null;
-		
+
 		if (j == i && j > 0 && event.cost >= 40)
 			event.cost = 39;
-		
+
 		if (event.cost >= 40) // && !thePlayer.capabilities.isCreativeMode
 			event.output = null;
-		
+
 		if (event.output != null) {
 			int l = event.output.getRepairCost();
 			if (event.output.hasDisplayName())
 				l = Math.max(0, l - 9);
 			l += 2;
-			
+
 			event.output.setRepairCost(l);
 			EnchantmentHelper.setEnchantments(enchs, event.output);
 		}
 	}
-	
+
 	private static int getCostPerLvl(int weight) {
 		switch (weight) {
 		case 1:
@@ -282,5 +282,5 @@ public class TomahawkImpls {
 			return 1;
 		}
 	}
-	
+
 }
